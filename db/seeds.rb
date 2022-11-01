c9ca27607bec50cd1ad8fee8a664d9eec653f8f9
JustinMitchell <mitchelljm04@gmail.com>
2022-11-01 14:26:02 -0400

    Clients Seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,21 @@
+puts "🌱 Seeding clients..."
 
+client1 = Client.create(
+    name: "United Illuminating",
+    contact: "203-909-3456",
+    manager: "John Mitchell"
+)
+
+client2 = Client.create(
+    name: "Deltek Products",
+    contact: "401-295-2525",
+    manager: "Richard Kazarian"
+)
+
+client3 = Client.create(
+    name: "Collins Construction",
+    contact: "203-956-2945",
+    manager: "Kevin Collins"
+)
 
 puts "✅ Done seeding!"
